6a797db2069cf4a2c3f0db9b64e3f10fe1faa073
midrees900 <midrees900@gmail.com>
2024-11-14 12:02:16 +0500

    routing2

--- app/login.tsx
+++ app/login.tsx
@@ -1,10 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useRouter } from 'expo-router'
+import MyButton from "@/components/MyButton"
 
 export default function Login() {
+  const router = useRouter();
+  const onLogin=()=>{
+    router.navigate("/signup")
+  }
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
+      
+      <MyButton title={"Login"} onPress={onLogin} />
     </View>
   )
 }
--- app/signup.tsx
+++ app/signup.tsx
@@ -1,10 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useRouter } from 'expo-router'
+import MyButton from '@/components/MyButton'
 
 export default function Signup() {
+
+  const router = useRouter()
+  const onRegister=()=>{
+    router.navigate("/login")
+
+  }
   return (
-    <View>
-      <Text>Signup</Text>
+    <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
+      
+      <MyButton title={"Register"} onPress={onRegister} />
     </View>
   )
 }
