5f65e844cc838f5d4b8411fb742ac8acabfb3384
midrees900 <midrees900@gmail.com>
2024-11-14 15:24:00 +0500

    login page ui

--- app/login.tsx
+++ app/login.tsx
@@ -1,19 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { useRouter } from 'expo-router'
-import MyButton from "@/components/MyButton"
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import { useRouter } from "expo-router";
+import MyButton from "@/components/MyButton";
+import { TextInput } from "react-native-gesture-handler";
 
 export default function Login() {
   const router = useRouter();
-  const onLogin=()=>{
-    router.navigate("/signup")
-  }
+  const onLogin = () => {
+    router.navigate("/signup");
+  };
   return (
-    <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
-      
-      <MyButton title={"Login"} onPress={onLogin} />
+    <View style={{ flex: 1 }}>
+      <Image
+        source={require("@/assets/images/login.jpg")}
+        style={{ width: "100%", height: 400 }}
+        resizeMode="cover"
+      />
+      <View style={{ padding: 20, gap: 20 }}>
+        <TextInput
+          placeholder="Enter Email Here"
+          style={{
+            borderWidth: 1,
+            height: 50,
+            paddingHorizontal: 20,
+            borderRadius: 10,
+          }}
+        />
+
+<TextInput
+          placeholder="Enter Password Here"
+          style={{
+            borderWidth: 1,
+            height: 50,
+            paddingHorizontal: 20,
+            borderRadius: 10,
+          }}
+        />
+
+        <MyButton title={"Login"} onPress={onLogin} />
+      </View>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- components/MyButton.tsx
+++ components/MyButton.tsx
@@ -1,31 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-export default function MyButton({title,onPress}) {
+export default function MyButton({ title, onPress }) {
   return (
     <View>
       {/* <Text>Continue</Text> */}
       {/* <Button title='Button'/> */}
-      <TouchableOpacity activeOpacity={0.8} style={styles.text} onPress={onPress}>
-      <Text style={styles.button}>{title}</Text> 
+      <TouchableOpacity
+        activeOpacity={0.8}
+       style={styles.button}
+        onPress={onPress}
+      >
+        <Text style={styles.text}>{title}</Text>
       </TouchableOpacity>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    button:{
-        backgroundColor:"orange",
-        paddingHorizontal:40,
-        paddingVertical:15,
-        borderRadius:10,
-    },
-    text:{
-        backgroundColor:"orange", 
-        paddingHorizontal:40, 
-        paddingVertical:15,
-        borderRadius:10
-
-    }
-})
+  button: {
+    backgroundColor: "orange",
+    paddingVertical: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 16,
+    color: "white",
+    fontWeight: "bold",
+  },
+});
