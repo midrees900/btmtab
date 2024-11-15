52d99dc5cbe749dc7596b56f0914239446046af3
midrees900 <midrees900@gmail.com>
2024-11-15 10:56:43 +0500

    register form

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 import { useRouter } from 'expo-router'
 import MyButton from '@/components/MyButton'
@@ -11,9 +11,36 @@ export default function Signup() {
 
   }
   return (
-    <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
+    <View style={{flex:1}}>
+        <Image
+        source={require("@/assets/images/Register.png")}
+        style={{ width: "100%", height: 400 }}
+        resizeMode="cover"
+      />
+<View style={{ padding: 20, gap: 20 }}>
+<TextInput
+          placeholder="Enter Email Here"
+          style={{
+            borderWidth: 1,
+            height: 50,
+            paddingHorizontal: 20,
+            borderRadius: 10,
+          }}
+          onChangeText={(e)=> console.log(e)}
+        />
+        <TextInput
+          placeholder="Enter Your Password"
+          style={{
+            borderWidth: 1,
+            height: 50,
+            paddingHorizontal: 20,
+            borderRadius: 10,
+          }}
+          onChangeText={(e)=> console.log(e)}
+        />
       
       <MyButton title={"Register"} onPress={onRegister} />
+      </View>
     </View>
   )
 }
